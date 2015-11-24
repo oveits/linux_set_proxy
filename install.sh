dbfb32d6794cb255eac61afab56f78e4080eaaaf
Oliver Veits <oliver.veits@web.de>
2015-11-24 09:39:17 +0100

    made install.sh portable for Cygwin/Mingw

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-sudo cp proxy /usr/bin/proxy
-sudo chmod 755 /usr/bin/proxy
+mingw=`uname -a | grep MINGW`
+
+if [ "$mingw" = "" ]; 
+then
+  sudo cp proxy /usr/local/bin/proxy
+  sudo chmod 755 /usr/local/bin/proxy
+else
+  # no sudo on cygwin/mingw:
+  cp proxy /bin/proxy
+  chmod 755 /bin/proxy
+fi
